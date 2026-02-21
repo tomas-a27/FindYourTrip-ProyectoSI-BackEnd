93a7521016bf4a154fecaa439651fbf781448f8e
meliaronson <melinaaronson@gmail.com>
2026-02-21 19:25:57 -0300

    CU02EditarPasajero (no probado)

--- src/usuario/usuario.routes.ts
+++ src/usuario/usuario.routes.ts
@@ -1,7 +1,8 @@
 import {Router} from 'express'
-import { sanitizeUsuarioInput, findOne, CU01RegistrarUsuario } from './usuario.controller.js'
+import { sanitizeUsuarioInput, findOne, CU01RegistrarUsuario, CU02EditarPasajero } from './usuario.controller.js'
 
 export const usuarioRouter = Router()
 
 usuarioRouter.get('/:id', findOne)
 usuarioRouter.post('/', sanitizeUsuarioInput, CU01RegistrarUsuario)
+usuarioRouter.put('/:id', sanitizeUsuarioInput, CU02EditarPasajero)

b9a4bf142fbc7b7b04b7ba7547d3487034bf2105
Candela Garcia <candegarciamo@gmail.com>
2026-03-11 18:23:48 -0300

    agg multer para fotos

--- src/usuario/usuario.routes.ts
+++ src/usuario/usuario.routes.ts
@@ -1,7 +1,8 @@
-import {Router} from 'express'
+import { Router } from 'express'
 import { sanitizeUsuarioInput, findOne, CU01RegistrarUsuario,
         CU02EditarPasajero, CU03SolicitarPasajeroComoConductor, loginUsuario,
         obtenerConductoresPendientes, CU04AprobarPasajeroComoConductor } from './usuario.controller.js'
+import { upload } from '../shared/multer.config.js' // <-- IMPORTAMOS MULTER
 
 export const usuarioRouter = Router()
 
@@ -9,6 +10,12 @@ usuarioRouter.post('/login', loginUsuario)
 usuarioRouter.get('/conductoresPendientes', obtenerConductoresPendientes)
 usuarioRouter.get('/:id', findOne)
 usuarioRouter.post('/', sanitizeUsuarioInput, CU01RegistrarUsuario)
-usuarioRouter.put('/solicitarSerConductor/:id', sanitizeUsuarioInput, CU03SolicitarPasajeroComoConductor)
+
+usuarioRouter.put('/solicitarSerConductor/:id', 
+upload.fields([{ name: 'fotoPerfil', maxCount: 1 }, { name: 'fotoLicencia', maxCount: 1 }]), 
+sanitizeUsuarioInput, 
+CU03SolicitarPasajeroComoConductor
+)
+
 usuarioRouter.put('/aprobarConductor/:id', sanitizeUsuarioInput, CU04AprobarPasajeroComoConductor)
 usuarioRouter.patch('/:id', sanitizeUsuarioInput, CU02EditarPasajero)

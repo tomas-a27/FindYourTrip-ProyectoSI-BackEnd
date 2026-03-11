6835b181c4ebca4efba538bf72d628d5f27eda18
Pedro Moretti Yrure <pedrocelumy@gmail.com>
2026-03-11 13:11:51 -0300

    CU17 Eliminar vehiculo finalizado

--- src/usuario/vehiculo.routes.ts
+++ src/usuario/vehiculo.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
-import { CU15CrearVehiculo, sanitizeVehiculoInput, CU16EditarVehiculo} from './vehiculo.controller.js'
+import { CU15CrearVehiculo, sanitizeVehiculoInput, CU16EditarVehiculo, CU17EliminarVehiculo} from './vehiculo.controller.js'
 export const vehiculoRouter = Router()
 
 vehiculoRouter.post('/:id', sanitizeVehiculoInput, CU15CrearVehiculo)
 vehiculoRouter.put('/:patente', sanitizeVehiculoInput, CU16EditarVehiculo)
+vehiculoRouter.delete('/:patente', CU17EliminarVehiculo)
